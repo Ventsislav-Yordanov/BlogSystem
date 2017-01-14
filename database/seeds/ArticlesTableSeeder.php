b2a52b46942fbeb9efc25d14ae6fb1bb79514417
venciity <venciityjordanov@gmail.com>
2017-01-14 16:39:49 +0200

    Added the seeder class for articles.

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 3; $i++) {
+            $article = new Article();
+            $article->title = 'Article ' . $i;
+            $article->content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem 
+            Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+            type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
+            versions of Lorem Ipsum.';
+            $article->save();
+        }
+    }
+}
